eccc1bb7521a927f9608003349b689602a2c5795
PinkHamsterOfMadness <pink.hamster.of.madness@gmail.com>
2019-03-28 21:16:05 +0300

    complete

--- Task.cs
+++ Task.cs
@@ -6,30 +6,45 @@ using System.Threading.Tasks;
 
 namespace Inheritance.MapObjects
 {
-    public class Dwelling
+    interface ICapture
+    {
+        int Owner { get; set; }
+    }
+
+    interface IFight
+    {
+        Army Army { get; set; }
+    }
+
+    interface ICollect
+    {
+        Treasure Treasure { get; set; }
+    }
+
+    public class Dwelling : ICapture
     {
         public int Owner { get; set; }
     }
 
-    public class Mine
+    public class Mine : ICapture, ICollect, IFight
     {
         public int Owner { get; set; }
         public Army Army { get; set; }
         public Treasure Treasure { get; set; }
     }
 
-    public class Creeps
+    public class Creeps : IFight, ICollect
     {
         public Army Army { get; set; }
         public Treasure Treasure { get; set; }
     }
 
-    public class Wolfs
+    public class Wolfs : IFight
     {
         public Army Army { get; set; }
     }
 
-    public class ResourcePile
+    public class ResourcePile : ICollect
     {
         public Treasure Treasure { get; set; }
     }
@@ -38,39 +53,14 @@ namespace Inheritance.MapObjects
     {
         public static void Make(Player player, object mapObject)
         {
-            if (mapObject is Dwelling)
-            {
-                ((Dwelling)mapObject).Owner = player.Id;
-                return;
-            }
-            if (mapObject is Mine)
-            {
-                if (player.CanBeat(((Mine)mapObject).Army))
+            if (mapObject is IFight)
+                if (!player.CanBeat((mapObject as IFight).Army))
                 {
-                    ((Mine)mapObject).Owner = player.Id;
-                    player.Consume(((Mine)mapObject).Treasure);
+                    player.Die();
+                    return;
                 }
-                else player.Die();
-                return;
-            }
-            if (mapObject is Creeps)
-            {
-                if (player.CanBeat(((Creeps)mapObject).Army))
-                    player.Consume(((Creeps)mapObject).Treasure);
-                else
-                    player.Die();
-                return;
-            }
-            if (mapObject is ResourcePile)
-            {
-                player.Consume(((ResourcePile)mapObject).Treasure);
-                return;
-            }
-            if (mapObject is Wolfs)
-            {
-                if (!player.CanBeat(((Wolfs)mapObject).Army))
-                    player.Die();
-            }
+            if (mapObject is ICapture) (mapObject as ICapture).Owner = player.Id;
+            if (mapObject is ICollect) player.Consume((mapObject as ICollect).Treasure);
         }
     }
 }
